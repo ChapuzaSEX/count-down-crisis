f9166f2e8b6e15b1cca28fd055d4022d3b1fc910
FelipeBlancoP <felipe.blanco@alumnos.ucn.cl>
2024-11-21 15:48:34 -0300

    Edificio Nivel 1

---
+++ ecenas/scrips/edificio.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var personaje = $CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_escalera_1_body_entered(body: Node2D) -> void:
+	personaje.position.x = 1369
+	personaje.position.y = 465
+
+
+func _on_escalera_2_body_entered(body: Node2D) -> void:
+	personaje.position.x = -103
+	personaje.position.y = 337
+
+
+func _on_escalera_3_body_entered(body: Node2D) -> void:
+	personaje.position.x = 1369
+	personaje.position.y = 209
+
+
+func _on_escalera_4_body_entered(body: Node2D) -> void:
+	personaje.position.x = -103
+	personaje.position.y = 81
+
+
+func _on_escalera_5_body_entered(body: Node2D) -> void:
+	personaje.position.x = 1369
+	personaje.position.y = -47
+
+
+func _on_escalera_6_body_entered(body: Node2D) -> void:
+	personaje.position.x = -103
+	personaje.position.y = -175
+
+
+func _on_escalera_7_body_entered(body: Node2D) -> void:
+	personaje.position.x = 1369
+	personaje.position.y = -303
