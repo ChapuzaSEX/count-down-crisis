b5bc83f00a6180374187d23ba6fde13673460282
FelipeBlancoP <felipe.blanco@alumnos.ucn.cl>
2024-12-10 20:42:49 -0300

    Segundo Nivel

---
+++ ecenas/scrips/edificio_segundo_nivel.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var personaje = $CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_escalera_12_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = 1369
+		personaje.position.y = 465
+
+
+func _on_escalera_22_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = -103
+		personaje.position.y = 337
+
+
+func _on_escalera_32_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = 1369
+		personaje.position.y = 209
+
+
+func _on_escalera_42_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = -103
+		personaje.position.y = 81
+
+
+func _on_escalera_52_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = 1369
+		personaje.position.y = -47
+
+func _on_escalera_62_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.nombre == "Alex Carter":
+		personaje.position.x = -103
+		personaje.position.y = -175
