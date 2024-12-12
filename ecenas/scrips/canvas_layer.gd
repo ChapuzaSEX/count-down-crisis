5706daba283b2491995ea03aabe945630a4d7066
juan3724x <juan3724x@gmail.com>
2024-12-12 14:41:03 -0300

    arreglo detalles de menu

--- ecenas/scrips/canvas_layer.gd
+++ ecenas/scrips/canvas_layer.gd
@@ -6,3 +6,7 @@ func _physics_process(delta):
 		get_tree().paused = not get_tree().paused
 		$ColorRect.visible = not $ColorRect.visible
 		$Label.visible = not $Label.visible
+		$salir.visible = not $salir.visible
+
+func _on_salir_pressed():
+	get_tree().change_scene_to_file("res://ecenas/menu_inicio.tscn")
--- ecenas/scrips/obpcion.gd
+++ ecenas/scrips/obpcion.gd
@@ -1,8 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 
 
 func _on_atras_pressed() -> void:
 	get_tree().change_scene_to_file("res://ecenas/menu_inicio.tscn")
+
+
+func _on_volumen_pressed() -> void:
+	get_tree().change_scene_to_file("res://ecenas/volumen.tscn")
